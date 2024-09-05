19a34145858bb195f51a1b196b810358c0fc27c1
KashiKalashetty <70686242+Kashi777@users.noreply.github.com>
2024-09-05 10:46:21 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -9,26 +9,3 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  // ASSERT_EQ(soundex,"A200");
 }
 
-TEST(SoudexTestsuite, AllLowerCaseCharacters) {
-  char soundex[5];
-  generateSoundex("kashi", soundex);
-  EXPECT_STREQ(soundex,"K000");
-}
-
-TEST(SoudexTestsuite, AllDigits) {
-  char soundex[5];
-  generateSoundex("12345", soundex);
-  EXPECT_STREQ(soundex,"1000");
-}
-
-TEST(SoudexTestsuite, NumberWithCharacters) {
-  char soundex[5];
-  generateSoundex("777ka", soundex);
-  EXPECT_STREQ(soundex,"7200");
-}
-
-TEST(SoudexTestsuite, NumberWithSpeacialCharacter) {
-  char soundex[5];
-  generateSoundex("7@$&*", soundex);
-  EXPECT_STREQ(soundex,"7000");
-}
