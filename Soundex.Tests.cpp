f64cedae668d53da14e205462564c538c510ef08
KashiKalashetty <70686242+Kashi777@users.noreply.github.com>
2024-09-05 09:58:51 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -5,5 +5,30 @@ TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
   generateSoundex("AX", soundex);
+ EXPECT_STREQ(soundex,"A200");
  // ASSERT_EQ(soundex,"A200");
+}
+
+TEST(SoudexTestsuite, AllLowerCaseCharacters) {
+  char soundex[5];
+  generateSoundex("kashi", soundex);
+  EXPECT_STREQ(soundex,"K000");
+}
+
+TEST(SoudexTestsuite, AllDigits) {
+  char soundex[5];
+  generateSoundex("12345", soundex);
+  EXPECT_STREQ(soundex,"1000");
+}
+
+TEST(SoudexTestsuite, NumberWithCharacters) {
+  char soundex[5];
+  generateSoundex("777ka", soundex);
+  EXPECT_STREQ(soundex,"7200");
+}
+
+TEST(SoudexTestsuite, NumberWithSpeacialCharacter) {
+  char soundex[5];
+  generateSoundex("7@$&*", soundex);
+  EXPECT_STREQ(soundex,"7000");
 }
