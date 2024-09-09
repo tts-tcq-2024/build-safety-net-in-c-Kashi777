476eef1c6c74bfb18c117d226a9ee57a4a687f3b
KashiKalashetty <70686242+Kashi777@users.noreply.github.com>
2024-09-09 23:53:10 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,11 +1,24 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
 
+#define MAX_CODE_LEN 5
+
+char soundex[MAX_CODE_LEN];
+
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
-  char soundex[5];
   generateSoundex("AX", soundex);
  EXPECT_STREQ(soundex,"A200");
  // ASSERT_EQ(soundex,"A200");
 }
 
+TEST(SoudexTestsuite, LowerCaseToUpeerCase) {
+ //AAA
+  generateSoundex("kashi", soundex);
+ EXPECT_STREQ(soundex,"K200");
+}
+TEST(SoudexTestsuite, PadsWithZeoIfStringIsLessThanFourCharacters) {
+ //AAA
+  generateSoundex("kk", soundex);
+  EXPECT_STREQ(soundex,"K200");
+}
